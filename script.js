d7994bdbb37c6e0d3ad9d37bf5db127733c687e9
jimmyMsh <103710156+jimmyMsh@users.noreply.github.com>
2023-12-12 20:23:40 -0500

    Start of next page

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('homeButton').addEventListener('click', function() {
     this.classList.add('clicked');
     setTimeout(() => {
         window.location.href = '/'; // Redirects to the homepage
-    }, 300); // Delay for animation
+    }, 700); // Delay for animation
 
     // Potential additional interactive feature:
     // For example, a toast message saying "Welcome back to the homepage!"
@@ -15,6 +15,34 @@ document.addEventListener('DOMContentLoaded', function() {
         welcomeScreen.classList.add('animate__animated', 'animate__fadeOut');
         setTimeout(function() {
             welcomeScreen.style.display = 'none';
-        }, 4000); // This controls the fade-out duration
+        }, 1000); // This controls the fade-out duration
     }, 500); // This controls how long the welcome screen is displayed
+});
+
+// Event listener for 'Home' button
+document.getElementById('homeButton').addEventListener('click', function() {
+    // Create a full-screen black overlay div
+    var blackOverlay = document.createElement('div');
+    blackOverlay.style.position = 'fixed';
+    blackOverlay.style.top = '0';
+    blackOverlay.style.left = '0';
+    blackOverlay.style.width = '100%';
+    blackOverlay.style.height = '100vh';
+    blackOverlay.style.backgroundColor = 'black';
+    blackOverlay.style.zIndex = '9999';
+    blackOverlay.style.opacity = '0';
+    blackOverlay.style.transition = 'opacity 0.5s ease';
+
+    // Append the black overlay to the body
+    document.body.appendChild(blackOverlay);
+
+    // Start the fade-in animation
+    setTimeout(() => {
+        blackOverlay.style.opacity = '1';
+    }, 10);
+
+    // After the animation, redirect to the next page
+    setTimeout(() => {
+        window.location.href = '/nextPage.html'; // Change to the actual URL of your next page
+    }, 2000); // This should be greater than the fade-in animation duration
 });
