13d1abf97074421fae2311de70f6b7ce3b96e3b2
jimmyMsh <103710156+jimmyMsh@users.noreply.github.com>
2023-12-12 20:57:07 -0500

    Final add

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// Event listener for 'Home' button
-document.getElementById('homeButton').addEventListener('click', function() {
-    this.classList.add('clicked');
-    setTimeout(() => {
-        window.location.href = '/'; // Redirects to the homepage
-    }, 700); // Delay for animation
-
-    // Potential additional interactive feature:
-    // For example, a toast message saying "Welcome back to the homepage!"
-});
 // Additional JS code for more interactivity can be added here
 document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
